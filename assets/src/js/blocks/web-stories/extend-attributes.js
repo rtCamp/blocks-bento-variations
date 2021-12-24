6edbd7f1b0f930c8e5c67e877199c26492d88762
RahiDroid <rahi.prajapati@rtcamp.com>
2021-12-24 14:51:52 +0530

    Feat: Register Web Stories block's bento variation

---
+++ assets/src/js/blocks/web-stories/extend-attributes.js
@@ -0,0 +1,43 @@
+/**
+ * WordPress dependencies.
+ */
+import { addFilter } from '@wordpress/hooks';
+
+/**
+ * Internal dependencies.
+ */
+import { BLOCK_NAME } from './constants';
+
+/**
+ * Function to extend attributes of the 'Web Stories' block.
+ *
+ * @param {Object} settings  settings object of the block.
+ * @param {string} blockName name of the block.
+ * @return {Object} settings object.
+ */
+const extendAttributes = (settings, blockName) => {
+	if (BLOCK_NAME !== blockName) {
+		return settings;
+	}
+
+	const newSettings = {
+		...settings,
+
+		attributes: {
+			...settings.attributes,
+
+			isBento: {
+				type: 'boolean',
+				default: false,
+			},
+		},
+	};
+
+	return newSettings;
+};
+
+addFilter(
+	'blocks.registerBlockType',
+	'blocks-bento-variations/extend-web-stories-attributes',
+	extendAttributes
+);
---
+++ assets/src/js/blocks/web-stories/index.js
@@ -0,0 +1,9 @@
+/**
+ * Entry point for all scripts responsible for Web Stories block's Bento variation.
+ */
+
+/**
+ * Internal dependencies.
+ */
+import './register-block-variation';
+import './extend-attributes';
